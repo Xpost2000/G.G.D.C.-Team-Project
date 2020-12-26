7837a2d45bc81000d4a651a0e8491111d7aca972
Xpost2000 <xpost2000cod@gmail.com>
2020-12-26 09:48:15 -0500

    Added script file for DialogueUI

---
+++ ui/DialogueUI.gd
@@ -0,0 +1,12 @@
+extends Control
+
+onready var dialogue_text = $DialogueBackground/DialogueText;
+onready var dialogue_speaker_name = $DialogueBackground/SpeakerTextLabelContainer/DialogueText;
+onready var dialogue_choices_container = $DialogueBackground/ChoicesContainer;
+onready var dialogue_continue_prompt = $DialogueBackground/ContinuePrompt;
+
+func _ready():
+	pass;
+
+func _process(delta):
+	pass;
